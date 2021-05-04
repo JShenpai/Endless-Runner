811be1ca986ab67bf39b2a52e412a0e897eaf2df
Jordan Shen <nehs.nadroj@gmail.com>
2021-05-03 23:43:22 -0700

    added description

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+/*
+COLLABORATORS: Jordan Shen, Ethan Chen, Mathew Aceves
+GAME TITLE: RESIST THE TEMPTATION
+DATE COMPLETED: 5/5/2021
+JUSTIFICATION:
+Varying the sizes of the platforms as well as the placement of sandwiches and cones using RNG is something we're proud of implementing!
+Making sure the player has a different experience each time is crucial when it comes to endless runners, so we're glad we were able
+to implement multiple random elements. Additionally we were able to implement a dash mechanic which helps increase the movement options
+in the game. This gives the player more skill expression in how they get from platform to platform while avoiding spikes.
+Our game's theme was created in section when all of us were hungry! With the player running away from the unhealthy food avalanche, I believe
+we did a great job depicting a dream world of fast food tempations in our sprite work!  
+*/
+
 let config =
 {
     type: Phaser.AUTO,
