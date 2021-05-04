a16703650e93ce02770af71fb0337f312710e2e6
Ethan Chen <ethchen@ucsc.edu>
2021-05-04 15:02:32 -0700

    slight wording change

--- src/scenes/Cutscene1.js
+++ src/scenes/Cutscene1.js
@@ -15,10 +15,11 @@ class Cutscene1 extends Phaser.Scene
     {
 
         this.add.tileSprite(0,0,640,480,'intro1').setOrigin(0,0);
-        this.add.text(20,20,"It's been a long day,");
-        this.add.text(20,40,"and you seem hungry...");
-        this.add.text(20,80,"Press SPACE to");
-        this.add.text(20,100,"continue");
+        this.add.text(20,20,"It's getting late,");
+        this.add.text(20,40,"and you seem");
+        this.add.text(20,60,"hungry...");
+        this.add.text(20,100,"Press SPACE to");
+        this.add.text(20,120,"continue");
         
         //define keys
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
